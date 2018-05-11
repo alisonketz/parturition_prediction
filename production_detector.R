c0defd67a3f1ebf451a78e3fb38c5b46179ee7a9
Ketz <ketza@DNR.state.wi.us>
2018-05-11 08:56:01 -0500

    Production run final, pw opens May 1

--- production_detector.R
+++ production_detector.R
@@ -9,11 +9,14 @@
 production = function(d,eps,pw=126){
 
   d=as.data.frame(d)
+
+  maxjulian = tail(d[,2],1)
+  todayjulian = yday(Sys.Date())
+  
   nCovs = dim(d)[2]-2
   id = unique(d[,1])
   nInd=length(id)
   ci = 3:dim(d)[2]
-  
   if(length(eps)!=nCovs){cat("length(epsilon) != number features, try again \n");return}
   
   #Check for individual case, run production detection on single individuals only
@@ -27,6 +30,8 @@ production = function(d,eps,pw=126){
       ind.sd =  sd(d[d[,2]<pw,ci],na.rm=TRUE)
       detect.quant = qnorm(eps,ind.mean,ind.sd)
       threshold.density = dnorm(detect.quant,ind.mean,ind.sd)
+      
+      d.temp[,ci] = -abs(d.temp[,ci])
       threshold.p = pnorm(detect.quant,mean=ind.mean,sd=ind.sd)
       lower.prob=pnorm(d.temp[,ci],mean=ind.mean,sd=ind.sd)
       detect.density = dnorm(d.temp[,ci],ind.mean,ind.sd)
@@ -37,11 +42,11 @@ production = function(d,eps,pw=126){
       results.prob=data.frame(matrix(NA,nr=n.temp,nc=4))
       names(results.prob)=c("Result","Prob","Julian","Date")
       results.prob[,3]=d.temp[,2]
-      results.prob[,4]=as.Date(d.temp[,2],origin="2018-01-01")
+      results.prob[,4]=as.Date(d.temp[,2],origin="2017-12-31")
       results.prob[hits.indx,1]="Hit"
       results.prob[outs.indx,1]="Out"
-      results.prob[hits.indx,2]=1-(lower.prob[hits.indx]/threshold.p)
-      results.prob[outs.indx,2] =1-(1-lower.prob[outs.indx])/(1-threshold.p)
+      results.prob[hits.indx,2]= 1-(lower.prob[hits.indx]/threshold.p)
+      results.prob[outs.indx,2] = 1-(threshold.p/lower.prob[outs.indx])
       
       for(i in 1:n.temp){
         if(results.prob[i,2]<0){
@@ -65,14 +70,14 @@ production = function(d,eps,pw=126){
       detect.density = rep(NA,n.temp)
       lower.prob=rep(NA,n.temp)
       for(i in 1:n.temp){
-          up=as.numeric(d.temp[i,ci])
+          up=-abs(as.numeric(d.temp[i,ci]))
           up.na=is.na(up)
           detect.density[i] = dmvnorm(up[!up.na],ind.mean[!up.na],diag(ind.sd[!up.na]))
           lower.prob[i]=pmvnorm(lower=rep(-Inf,nCovs-sum(up.na)),upper = up[!up.na],mean=ind.mean[!up.na],sigma=diag(ind.sd[!up.na]))[1]
       }
       
-      # hits.indx = which(is.finite(detect.density) & detect.density<=threshold.density & lower.prob <= threshold.p)
-      hits.indx = which(is.finite(detect.density) & detect.density<=threshold.density)
+      hits.indx = which(is.finite(detect.density) & detect.density<=threshold.density & lower.prob <= threshold.p)
+      # hits.indx = which(is.finite(detect.density) & detect.density<=threshold.density)
       
       outs.indx = which(!(1:n.temp %in% hits.indx))
       alarm=list(rbind(d.temp[hits.indx,]))
@@ -80,22 +85,37 @@ production = function(d,eps,pw=126){
       results.prob=data.frame(matrix(NA,nr=n.temp,nc=4))
       names(results.prob)=c("Result","Prob","Julian","Date")
       results.prob[,3]=d.temp[,2]
-      results.prob[,4]=as.Date(d.temp[,2],origin="2018-01-01")
+      results.prob[,4]=as.Date(d.temp[,2],origin="2017-12-31")
       results.prob[hits.indx,1]="Hit"
       results.prob[outs.indx,1]="Out"
       results.prob[hits.indx,2]=1-(lower.prob[hits.indx]/threshold.p)
-      results.prob[outs.indx,2] =1-(1-lower.prob[outs.indx])/(1-threshold.p)
+      results.prob[outs.indx,2] =1-(threshold.p/lower.prob[outs.indx])
       
-      for(i in 1:n.temp){
-        if(results.prob[i,2]<0){
-          results.prob[i,2] =1-(1-lower.prob[i])/(1-threshold.p)
-          results.prob[i,1] ="Out*"
-        }#end if
-      }#end for
+      # update=which(results.prob[,2]<0)
+      
+      # for(i in 1:n.temp){
+      #   if(results.prob[i,2]<0){
+      #     results.prob[i,2] =1-(1-lower.prob[i])/(1-threshold.p)
+      #     results.prob[i,1] ="Out*"
+      #   }#end if
+      # }#end for
+
     }#end else nCovs>1,nInd = 1
 
-    hit.today=results.prob[n.temp,1]=="Hit"
+    if(maxjulian==todayjulian | maxjulian==(todayjulian-1)){
+      hit.today=results.prob[n.temp,1]=="Hit"}
+    else{hit.today=FALSE}
+  
+    # if(maxjulian==todayjulian | maxjulian==(todayjulian-1) & max(results.prob[update,3])!=todayjulian){
+    #   hit.today=results.prob[n.temp,1]=="Hit"}
+    # else{hit.today=FALSE}
 
+    alarm = as.data.frame(alarm)
+    # alarm.rm=which(results.prob[update,3] %in% alarm[,2])
+    # alarm=alarm[-alarm.rm,]
+    
+    # hit.indx=hit.indx!(which(update %in% hit.indx))
+  
     return(list(results.prob=results.prob,
                 detect.quant=detect.quant,
                 threshold.density=threshold.density,
@@ -105,6 +125,7 @@ production = function(d,eps,pw=126){
                 hits.indx=hits.indx,
                 outs.indx=outs.indx,
                 hit.today=hit.today,
-                id=id))
+                id=id,
+                alarm=alarm))
   
 }#end production function
--- production_run.R
+++ production_run.R
@@ -32,10 +32,11 @@ library(adehabitatLT)
 library(geosphere)
 library(mvtnorm)
 library(xtable)
-library(mailR)
 library(rmarkdown)
 library(ggplot2)
 library(rgdal)
+library(pander)
+library(gmailr)
 
 setwd("C:/Users/ketza/Documents/parturition_prediction")
 
@@ -80,11 +81,42 @@ ids=unlist(lapply(datafiles,function(x){x[1,1]}))
 #remove morts from overall list of dataframes
 datafiles[which(ids %in% morts)]=NULL
 
+
 #get list of remaining alive ids
 ids=unlist(lapply(datafiles,function(x){x[1,1]}))
 
-datafiles=lapply(datafiles,function(x){
 
+#remove individuals with messed up GPS collars
+badcollars=c(5884,6044,7237,5173,5107,5740,5078,6884,5900,5720,6847,6821)
+
+datafiles[which(ids %in% badcollars)]=NULL
+
+#get list of remaining individuals to run through detector
+ids=unlist(lapply(datafiles,function(x){x[1,1]}))
+
+miss.long=sapply(datafiles,function(x){sum(x$longitude==0)})
+miss.lat = sapply(datafiles,function(x){sum(x$latitude==0)})
+
+miss.gps=data.frame(ids,miss.long)
+save(miss.gps,file=paste("Results/",Sys.Date(),"-",format(Sys.time(),"%p"),"-miss.gps.Rdata",sep=""))
+
+#removing bad GPS fixes where DOP >10
+datafiles=lapply(datafiles,function(x){
+  x=x[x$dop<10,]
+  x
+})
+
+x=datafiles[[1]]
+miss.latest=sapply(datafiles,function(x){
+  x$date_time_local=as.POSIXct(x$datetimegmt*60*60*24, tz = "CST6CDT", origin = "1899-12-30")
+  x$julian=yday(x$date_time_local)
+  temp=tail(unique(x$julian),2)
+  y=sum(x[x$julian==temp[1] |x$julian==temp[2],7]==0)
+  y
+})
+
+datafiles=lapply(datafiles,function(x){
+  
     #remove observations with 0 long/lat
     x=x[x$longitude!=0,]
     x=x[x$latitude!=0,]
@@ -138,7 +170,16 @@ sizes=lapply(datafiles,function(x){
   dim(x)[1]
 })
 sizes=unlist(sizes)
+
+removed.indx=which(sizes==0)
+removed=ids[removed.indx]
+removed 
 datafiles[which(sizes==0)]=NULL
+miss.latest=miss.latest[-removed.indx]
+ids=ids[-removed.indx]
+
+save(removed,file=paste("Results/",Sys.Date(),"-",format(Sys.time(),"%p"),"-removed.init.Rdata",sep=""))
+
 
 #project data 
 #calculate and compile adehabitat features
@@ -179,7 +220,6 @@ datafiles=lapply(datafiles,function(x){
     x
 })
 
-
 #two day moving window summary statistics, centered and scaled, returns list of matrixes of all the features
 #first column is id
 #second column is julian day
@@ -211,14 +251,29 @@ features=lapply(datafiles,function(x){
     x.covs
 })
 
+nInd.collared=length(features)
 
+#parturition window date
+#May 1
+pw.julian=yday("2018-05-01")
 
 ### remove individuals without observations since spring start date
 maxdates=sapply(features,function(x){
   max(x[,2])
 })
-maxdatess=unlist(maxdates)
-features[which(maxdates<105)]=NULL
+maxdates=unlist(maxdates)
+
+missed.latest=miss.latest[-which(maxdates<pw.julian)]
+id.notrun=sapply(features[which(maxdates<pw.julian)],function(x){
+	x[1,1]})
+date.notrun=as.character(as.Date(maxdates[which(maxdates<pw.julian)],origin="2017-12-31"))
+
+notrun.df=data.frame(id.notrun,date.notrun)
+save(notrun.df,file=paste("Results/",Sys.Date(),"-",format(Sys.time(),"%p"),"-notrun.df.Rdata",sep=""))
+
+features[which(maxdates<pw.julian)]=NULL
+nInd.run = length(features)
+
 
 load('epsilon.Rdata')
 
@@ -227,29 +282,25 @@ load('epsilon.Rdata')
 ### easily extendable to all individuals
 ###
 
-yday("2018-04-15")
-# [1] 105
+# yday("2018-05-10) #this will be the parturition window julian day
 
 source("production_detector.R")
+
 out=lapply(features,function(x){
-  production(x,eps=epsilon,pw=105)
+  production(x,eps=epsilon,pw=pw.julian)
 })
 
 hits = sapply(out,function(x){x$hit.today})
 hits.today = ifelse(hits,'Hit','Out')
 results.prob.sum = sapply(out,function(x){tail(x$results.prob,1)[,2]})
 threshold.p.sum = sapply(out,function(x){x$threshold.p})
+lower.p.sum = sapply(out,function(x){tail(x$lower.prob,1)})
+last.julian.obs = sapply(out,function(x){tail(x$results.prob,1)[,3]})
+last.date.obs=sapply(out,function(x){y=tail(x$results.prob,1)[,4];as.character(y)})
 id = as.factor(sapply(out,function(x){x$id}))
 
-par(mfrow=c(1,1))
-out.df=data.frame(id,hits.today,results.prob.sum,threshold.p.sum)
-pdf(paste(Sys.Date(),"-results-plot.pdf",sep=""))
-ggplot(data=out.df,aes(x=id))+geom_point(aes(y=results.prob.sum,color=hits))+
-  theme(panel.background = element_rect(fill="white",color="black"),
-        panel.grid.major=element_line("grey90"),
-        axis.text.x = element_text(angle=90))
-dev.off()
 
+#email body text output
 
 if(sum(hits)==0){
   body.out="No Predicted Births Today"
@@ -257,38 +308,124 @@ if(sum(hits)==0){
   body.out= paste("Lowtag of predicted births: \n",paste(as.character(id[hits]),collapse="\n"),sep="")
 }
 
-save(out.df,file=paste("Results/",Sys.Date(),"-resultsout.Rdata",sep=""))
+
+#data frame subsets of results for output in report
+
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+pw = yday("2018-05-01")
+julian.today = yday(Sys.Date())
+end = yday("2018-06-30")
+Day = rep(pw:end,nInd.run)
+ID = sort(rep(id,length(pw:end)))
+
+
+hit.df=data.frame(Day,ID)
+hit.mat = matrix(0,ncol=(end-pw+1),nrow=nInd.run)
+for(j in 1:nInd.run){
+    if(dim(out[[j]]$alarm)[1]!=0){
+      hit.mat[j,(out[[j]]$alarm[,2] - pw+1)] = 1
+    }
+}
+
+hit.mat=cbind(as.numeric(as.character(id)),hit.mat)
+hit.mat=hit.mat[order(hit.mat[,1]),]
+hit.mat=hit.mat[,-1]
+
+face=c()
+for(k in 1:floor(nInd.run/30)){
+  face=c(face,rep(k,30*length(pw:end)))
+}
+face=c(face,rep(floor(nInd.run/30)+1,(nInd.run%%30)*length(pw:end)))
+
+hit.df$face=face
+hit.df$Hit = c(t(hit.mat))
+hit.df$IDnumeric=hit.df$ID
+hit.df$ID = as.factor(hit.df$ID)
+
+save(hit.df,file=paste("Results/",Sys.Date(),"-",format(Sys.time(),"%p"),"-hit.df.Rdata",sep=""))
+
+length(id)
+length(missed.latest)
+
+#dataframe output of anomaly detection results for individuals run
+out.df=data.frame(id,hits.today,last.julian.obs,last.date.obs,results.prob.sum,threshold.p.sum,lower.p.sum,missed.latest,stringsAsFactors = FALSE)
+
+#subset of individuals who's latest observation was not today's date, or yesterdays date
+not.out.df = out.df[out.df$last.julian.obs!=julian.today & out.df$last.julian.obs!=(julian.today-1),]
+
+#fixing and formatting out.df
+hits.out=hits[out.df$last.julian.obs==julian.today| out.df$last.julian.obs==(julian.today-1)]
+out.df = out.df[out.df$last.julian.obs==julian.today| out.df$last.julian.obs==(julian.today-1),]
+out.df$hits.today=as.factor(out.df$hits.today)
+out.df$id=as.factor(out.df$id)
+
+#fixing and formatting not.out.df
+#length(out[which(id %in% not.out.df$id)])
+latest.result=lapply(out[which(id %in% not.out.df$id)],function(x){
+  tail(x$results.prob,1)[1]
+})
+latest.result=as.character(unlist(latest.result))
+not.out.df$hits.today=latest.result
+
+save(out.df,file=paste("Results/",Sys.Date(),"-",format(Sys.time(),"%p"),"-resultsout.Rdata",sep=""))
+save(not.out.df,file=paste("Results/",Sys.Date(),"-",format(Sys.time(),"%p"),"-notresultsout.Rdata",sep=""))
+
+###
+### If individuals were out today due to no data even though they were run through the anomaly detector, then here are the results in plot form for the day before today
+### The following is a summary of recent results of those individuals
+
+
+# alarm.indx=which(id %in% not.out.df[,1])
+# alarm.out=do.call(rbind,lapply(out[alarm.indx],function(x){x$alarm}))
+# subs=which(alarm.out[,2]==(julian.today-2))
+# results.prob.out=lapply(out[alarm.indx],function(x){x$results.prob})
+# results.prob.out[!(1:length(alarm.indx) %in% subs)]=NULL
+# lower.prob.out=lapply(out[alarm.indx],function(x){tail(x$lower.prob,1)})
+# lower.prob.out[!(1:length(alarm.indx) %in% subs)]=NULL
+# lp=do.call(rbind,lower.prob.out)
+# thresh.prob.out=lapply(out[alarm.indx],function(x){x$threshold.p})
+# thresh.prob.out[!(1:length(alarm.indx) %in% subs)]=NULL
+# th.p=do.call(rbind,thresh.prob.out)
+# priorday=cbind(alarm.out[subs,1],do.call(rbind,lapply(results.prob.out,function(x){tail(x,1)})),lp,th.p)
+# if(dim(priorday)[1]!=0){
+#   names(priorday)=c("ID","Result","Prob","Julian","Date","lower.prob","th.p")
+#   priorday=priorday[order(priorday$ID),]
+# }else{
+#   priorday = "No prior day hits to show"
+# }
+# 
+# save(priorday,file=paste("Results/",Sys.Date(),"-",format(Sys.time(),"%p"),"-priorday.Rdata",sep=""))
+# 
 
 ########################################################################################
 ###
-### Render html document and email results
+### Render pdf document and email results
 ###
 ###
 ########################################################################################
 
-rmarkdown::render("Report_Generation.Rmd",output_file=paste(Sys.Date(),"-Prediction-Report.pdf",sep=""))
+rmarkdown::render("Report_Generation.Rmd",output_file=paste(Sys.Date(),"-",format(Sys.time(),"%p"),"-Prediction-Report.pdf",sep=""),output_dir = "Results")
 
-# sender <- "alison.ketz@gmail.com" 
-# recipients <- c("alison.ketz@gmail.com","aketz@wisc.edu") 
-# email <- send.mail(from = sender,
-#                    to = recipients,
-#                    subject=paste("Prediction Results",Sys.Date()),
-#                    body = print(body.out),
-#                    smtp = list(host.name = "aspmx.l.google.com", port = 25),
-#                    authenticate = FALSE,
-#                    attach.files = paste(getwd(),"/",Sys.Date(),"-Prediction-Report.pdf",sep="") ,
-#                    debug=FALSE,
-#                    send = TRUE)
+html_msg_attach <- mime() %>%
+  to(c("aketz@wisc.edu",
+	"DanielJ.Storm@wisconsin.gov",
+	"dwalsh@usgs.gov",
+	"Wesley.Ellarson@wisconsin.gov",
+	"Katie.Luukkonen@wisconsin.gov",
+	"Logan.Hahn@wisconsin.gov",
+	"Dana.Jarosinski@wisconsin.gov",
+	"Hannah.Manninen@wisconsin.gov")) %>%
+  from("wtdparturition@gmail.com") %>%
+  subject(paste("Prediction Results",Sys.Date(),"-",format(Sys.time(),"%p"))) %>%
+  html_body(print(body.out))%>%
+  attach_part(print(body.out)) %>%
+  attach_file(paste("C:/Users/ketza/Documents/parturition_prediction/Results/",Sys.Date(),"-",format(Sys.time(),"%p"),"-Prediction-Report.pdf",sep=""))
 
-sender <- "aketz@wisc.edu" 
-recipients <- c("alison.ketz@gmail.com","aketz@wisc.edu") 
-sapply(recipients,function(x){send.mail(from = sender,
-                   to = x,
-                   subject=paste("Prediction Results",Sys.Date()),
-                   body = print(body.out),
-                   smtp = list(host.name = "smtp.office365.com",port=587,user.name="aketz@wisc.edu",passwd="Recess$5505",tls=TRUE),
-                   authenticate = TRUE,
-                   attach.files = paste("C:/Users/ketza/Documents/parturition_prediction/Results/",Sys.Date(),"-Prediction-Report.pdf",sep="") ,
-                   debug=FALSE,
-                   send = TRUE)})
+send_message(html_msg_attach)
 
+
+###
+### Email addresses to send prediction results report
+###
+#	
